671c8d9483174394f5495e26f5b7e0e2ae47383e
evilgrou-tech <evilgrou@gmail.com>
2026-02-16 07:50:33 +0300

    Add files via upload

---
+++ calculator.ps1
@@ -0,0 +1,5 @@
+# Тестовый скрипт для открытия калькулятора
+Write-Host "Запуск калькулятора..." -ForegroundColor Green
+Start-Process "calc.exe"
+Write-Host "Калькулятор успешно запущен!" -ForegroundColor Green
+Start-Sleep -Seconds 2
